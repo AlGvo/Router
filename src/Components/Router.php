c35ae6bf6d7b2bf72591cb5ba2cfcb27be18ac61
Alexander Gvozdev <dp161185gav@gmail.com>
2022-05-11 19:49:27 +0300

    Initial Custom Router

---
+++ src/Components/Router.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Algvo\Router\Components;
+
+
+class Router implements \Aigletter\Contracts\Routing\RouteInterface
+{
+
+
+    private string $namespace;
+
+    public function __construct(string $namespace)
+    {
+        $this->namespace = $namespace;
+    }
+
+    public function route(string $uri): callable
+    {
+        $segments = explode("/", $uri);
+        array_shift($segments);
+        $controller = $this->namespace. ucfirst($segments[0]);
+        $method = $segments[1];
+
+        if (class_exists($controller) && isset($method) && method_exists($controller, $method)) {
+            return function () use ($controller, $method) {
+                return call_user_func([$controller, $method]);
+            };
+        }
+
+        http_response_code(404);
+        echo '404 Not found';
+        die();
+    }
+}

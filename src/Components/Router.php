efc35df1732e032f7278dda55634b8d9b37ac9b8
Alexander Gvozdev <dp161185gav@gmail.com>
2022-05-14 18:42:24 +0300

    Add id in to the statement

--- src/Components/Router.php
+++ src/Components/Router.php
@@ -24,7 +24,10 @@ class Router implements \Aigletter\Contracts\Routing\RouteInterface
 
         if (class_exists($controller) && isset($method) && method_exists($controller, $method)) {
             return function () use ($controller, $method) {
-                return call_user_func([$controller, $method]);
+               // return call_user_func([$controller, $method]);
+                $callback = [$controller, $method];
+                $id = $_GET['id'];
+                $callback($id);
             };
         }
 

220ea612536af14f05177778be24843b136e4772
Alexander Gvozdev <dp161185gav@gmail.com>
2022-05-15 20:05:15 +0300

    Add reflection

--- src/Components/Router.php
+++ src/Components/Router.php
@@ -17,22 +17,45 @@ class Router implements \Aigletter\Contracts\Routing\RouteInterface
 
     public function route(string $uri): callable
     {
+        $uri = substr($uri, 0, strpos($uri, "?"));
         $segments = explode("/", $uri);
         array_shift($segments);
         $controller = $this->namespace. ucfirst($segments[0]);
         $method = $segments[1];
-
         if (class_exists($controller) && isset($method) && method_exists($controller, $method)) {
+            $controller = new $controller();
             return function () use ($controller, $method) {
-               // return call_user_func([$controller, $method]);
                 $callback = [$controller, $method];
-                $id = $_GET['id'];
-                $callback($id);
+                $reflectionMethod = new \ReflectionMethod($controller, $method);
+                $arguments = $this->resolveParameters($reflectionMethod);
+                $reflectionMethod->invokeArgs($controller, $arguments);
             };
         }
-
         http_response_code(404);
         echo '404 Not found';
         die();
+    }
+    protected function resolveParameters($reflectionMethod)
+    {
+
+        foreach ($reflectionMethod->getParameters() as $parameters) {
+            $name = $parameters->getName();
+            $type = $parameters->getType();
+            if  ($type && !$type->isBuiltin()) {
+                $className = $type->getName();
+                $value = new $className();
+            } else {
+                if (!isset($_GET[$name])) {
+                    continue;
+                }
+                echo 'Name'. $name . PHP_EOL;
+                $value = $_GET[$name];
+                if ($type && $type->getName() !== gettype($value)) {
+                    settype($value, $type->getName());
+                }
+            }
+            $arguments[$name] = $value;
+        }
+        return $arguments;
     }
 }
